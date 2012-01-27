24a4fed2f7ef0005aa0e2c4f3596658bed2854e2
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2012-01-27 13:16:58 +0100

    Cache JSONP page

--- mbtilesmap/__init__.py
+++ mbtilesmap/__init__.py
@@ -4,9 +4,10 @@ from easydict import EasyDict
 
 MBTILES_NAME_PATTERN = r'[\.\-\w]+'
 
-app_settings = EasyDict(dict({
-    'MAP_URL_NAME' : 'map',
-    'MBTILES_EXT' : 'mbtiles',
-    'MBTILES_ROOT' : settings.MEDIA_ROOT,
-    'TILE_SIZE' : 256,
-}, **getattr(settings, 'MBTILES_APP_CONFIG', {})))
+app_settings = EasyDict(dict(
+    MAP_URL_NAME = 'map',
+    MBTILES_EXT = 'mbtiles',
+    MBTILES_ROOT = settings.MEDIA_ROOT,
+    TILE_SIZE = 256,
+    CACHE_TIMEOUT_JSONP = 60 * 60,  # 1 hour
+), **getattr(settings, 'MBTILES_APP_CONFIG', {}))
--- mbtilesmap/views.py
+++ mbtilesmap/views.py
@@ -2,7 +2,9 @@ import logging
 
 from django.http import Http404, HttpResponse
 from django.utils.translation import ugettext as _
+from django.views.decorators.cache import cache_page
 
+from . import app_settings
 from models import MBTiles, MissingTileError, MBTilesNotFoundError
 
 
@@ -24,6 +26,7 @@ def tile(request, name, z, x, y):
     raise Http404
 
 
+@cache_page(app_settings.CACHE_TIMEOUT_JSONP)
 def jsonp(request, name):
     """ Serve the map configuration as JSONP """
     mbtiles = MBTiles(name)

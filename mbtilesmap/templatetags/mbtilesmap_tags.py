3a21470368f18c9fc61e96f4958d2dda8314d7c4
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-09-13 12:15:02 +0200

    SIM-PLI-FY template tags

--- mbtilesmap/templatetags/mbtilesmap_tags.py
+++ mbtilesmap/templatetags/mbtilesmap_tags.py
@@ -6,37 +6,13 @@ from mbtilesmap.models import MBTiles
 register = template.Library()
 
 
-@register.simple_tag
+@register.inclusion_tag('mbtilesmap/head.html')
 def mbtilesmap_head():
-    c = template.Context()
-    return template.loader.get_template('mbtilesmap/head.html').render(c)
+    return {}
 
 
-class MapNode(template.Node):
-    def __init__(self, name):
-        self.name = template.Variable(name)
-        self.mbtiles = None
-
-    def render(self, context):
-        name = self.name.resolve(context)
-        self.mbtiles = MBTiles(name)
-
-        t = template.loader.get_template('mbtilesmap/map.html')
-
-        c = template.Context({'map': self.mbtiles},
-                             autoescape=context.autoescape)
-        return t.render(c)
-
-
-@register.tag(name="mbtilesmap")
-def do_mbtilesmap(parser, token):
-    """
-    {% mbtiles_map name %}
-
-    Renders a map for specified MBTiles file
-    """
-    args = token.split_contents()
-    if len(args) < 2:
-        raise template.TemplateSyntaxError('mbtiles_map takes at least one '
-                                           'argument')
-    return MapNode(args[1])
+@register.inclusion_tag('mbtilesmap/map.html')
+def mbtilesmap(name, catalog=None):
+    mbtiles = MBTiles(name, catalog=catalog)
+    return {'catalog': catalog,
+            'map': mbtiles}

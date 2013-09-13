d186abeeca3e2f604a8ef989eef924ed8ee0bffe
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-09-13 12:21:43 +0200

    Back to development: 1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-mbtiles',
-    version='1.2',
+    version='1.3.dev0',
     author='Mathieu Leplatre',
     author_email='mathieu.leplatre@makina-corpus.com',
     url='https://github.com/makinacorpus/django-mbtiles',

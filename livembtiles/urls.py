e2d8d94134e551141f892e28e97dab06c2923c8e
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-09-18 10:21:41 +0200

    Add JSON version of catalog

--- livembtiles/urls.py
+++ livembtiles/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 
 from mbtilesmap import MBTILES_CATALOG_PATTERN, MBTILES_ID_PATTERN
-from .views import index, catalog, preview
+from .views import index, catalog, catalog_json, preview
 
 
 urlpatterns = patterns('',
     url(r'^$', index, name="index"),
     url(r'^(?P<catalog>%s)/$' % MBTILES_CATALOG_PATTERN, catalog, name="catalog"),
+    url(r'^(?P<catalog>%s).json$' % MBTILES_CATALOG_PATTERN, catalog_json, name="catalog_json"),
     url(r'^(?P<name>%s)/map/$' % (MBTILES_ID_PATTERN), preview, name="map"),
     url(r'^(?P<catalog>%s)/(?P<name>%s)/map/$' % (MBTILES_CATALOG_PATTERN, MBTILES_ID_PATTERN), preview, name="map"),
     url(r'^', include('mbtilesmap.urls', namespace='mb', app_name='mbtilesmap')),
--- livembtiles/views.py
+++ livembtiles/views.py
@@ -1,5 +1,9 @@
+import json
+
 from django.shortcuts import redirect
+from django.http import HttpResponse
 from django.views.generic import ListView, TemplateView
+from django.core.urlresolvers import reverse
 from django.http import Http404
 
 from mbtilesmap.models import MBTiles, MBTilesNotFoundError
@@ -30,7 +34,38 @@ class CatalogView(ListView):
         except MBTilesNotFoundError:
             raise Http404
 
+
+class JSONResponseMixin(object):
+    def render_to_response(self, context, **response_kwargs):
+        return HttpResponse(
+            self.convert_context_to_json(context),
+            content_type='application/json',
+            **response_kwargs
+        )
+
+    def convert_context_to_json(self, context):
+        catalog = self.kwargs.get('catalog')
+        maps = []
+        for _map in context['object_list']:
+            tilejsonpath = reverse('mbtilesmap:tilejson', kwargs=dict(catalog=catalog, name=_map.id))
+            tilejsonurl = self.request.build_absolute_uri(tilejsonpath)
+            previewpath = reverse('mbtilesmap:preview', kwargs=dict(catalog=catalog, name=_map.id))
+            previewurl = self.request.build_absolute_uri(previewpath)
+            attrs = dict(id=_map.id,
+                         name=_map.name,
+                         preview=previewurl,
+                         tilejson=tilejsonurl,
+                         metadata=_map.metadata)
+            maps.append(attrs)
+        return json.dumps(dict(maps=maps))
+
+
+class CatalogViewJSON(JSONResponseMixin, CatalogView):
+    pass
+
+
 catalog = CatalogView.as_view()
+catalog_json = CatalogViewJSON.as_view()
 
 
 class PreviewView(TemplateView):
